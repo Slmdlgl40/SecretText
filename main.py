31473d4137aa7ff5eb700e8d188a8054bd55f5cf
Salim Dalgalı <sdalgali03@gmail.com>
2023-07-12 08:19:14 -0500

    kullanıcı arayüzüne görsel eklendi

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
 from tkinter import *
-
-def size(x):
-    x.update()
-    print(x.winfo_height())
-    print(x.winfo_width())
+from PIL import Image, ImageTk
 
 window = Tk()
-window.minsize(width=400,height=600)
+window.minsize(width=400,height=700)
 window.title("Secret Notes")
 
+image = Image.open("image.jpg")
+photo_width = 100
+photo_height = 100
+resize = image.resize((photo_width,photo_height))
+photo = ImageTk.PhotoImage(resize)
+
+photo_label = Label(image=photo)
+photo_label.place(x=150,y=50)
+
 label_1 = Label(text="Enter your title",font=("Arial",15,"bold"))
-label_1.place(x=127,y=90)
+label_1.place(x=127,y=190)
 
 title_entry = Entry(width=20)
-title_entry.place(x=107,y=115)
+title_entry.place(x=107,y=215)
 
 label_2 = Label(text="Enter your secret",font=("Arial",15,"bold"))
-label_2.place(x=117,y=140)
+label_2.place(x=117,y=240)
 
 secret_text = Text(width=40,height=15)
-secret_text.place(x=37,y=165)
+secret_text.place(x=37,y=265)
 
 label_3 = Label(text="Enter master key",font=("Arial",15,"bold"))
-label_3.place(x=117,y=460)
+label_3.place(x=117,y=560)
 
 key_entry = Entry(width=20)
-key_entry.place(x=107,y=490)
+key_entry.place(x=107,y=590)
 
 encrypt_button = Button(text="Save & Encrypt",width=10)
-encrypt_button.place(x=141,y=520)
+encrypt_button.place(x=141,y=620)
 
 decrypt_button = Button(text="Decrypt",width=10)
-decrypt_button.place(x=141,y=550)
+decrypt_button.place(x=141,y=650)
 
 window.mainloop()
 
